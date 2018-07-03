6fd536bdede19f5e2720ac4ab517145511af07f5
Arsenii Semenov <tyranid3r@gmail.com>
2018-07-03 13:51:50 +0300

    Delete json

--- schedule.py
+++ schedule.py
@@ -1,4 +1,3 @@
-import json
 import requests
 from apiclient.discovery import build
 from httplib2 import Http
@@ -15,7 +14,7 @@ reminders_type = 'popup' #popup or email
 
 # Getting timetable
 response = requests.get('https://scheduler.talantiuspeh.ru/app.php/meeting/display?name=%D0%9D%D0%B0%D1%83%D0%BA%D0%B0')
-data = json.loads(response.text)
+data = response.json()
 meetings = data[team_id-1]['meetings']
 
 # Setup the Calendar API
